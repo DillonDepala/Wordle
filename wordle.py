c0bf0c20a7cbdf8a9c1aac41f3ed1c1e33487e36
DillonDepala <dillondepala@hotmail.com>
2022-12-23 16:33:53 +0000

    Created user_imput func with error handling

--- wordle.py
+++ wordle.py
@@ -1,12 +1,19 @@
 import random
-import re
 import numpy as np
-from constants import WORD_LENGTH
+from constants import WORD_LENGTH, ACCEPTABLE_WORDS_LIST
 import requests
+
+# BLANK_ARRAY = np.array()
 
 def user_input():
     user_choice = input(f'Choose a {WORD_LENGTH} letter word: ').lower()
-    return user_choice
+    if len(user_choice) > WORD_LENGTH:
+        raise Exception(f"Sorry only {WORD_LENGTH} letter words are allowed\n Try again")
+    elif user_choice not in ACCEPTABLE_WORDS_LIST:
+        raise Exception("Not a valid word\n please try again")
+    else:
+        return user_choice
+
 
 user_input()
 
