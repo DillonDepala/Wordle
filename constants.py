e3a0cdce19216e80e387e97affad0f98d1cfdfd3
DillonDepala <dillondepala@hotmail.com>
2022-12-23 16:35:16 +0000

    Created an acceptable word list constant

--- constants.py
+++ constants.py
@@ -1,1 +1,9 @@
+import re
+import requests
 WORD_LENGTH = 5
+
+meaningpedia_resp = requests.get(
+    "https://meaningpedia.com/5-letter-words?show=all")
+pattern = re.compile(r'<span itemprop="name">(\w+)</span>')
+
+ACCEPTABLE_WORDS_LIST = pattern.findall(meaningpedia_resp.text)
