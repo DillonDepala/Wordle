d0d257f0b6237c38a3feabce98ea99994b6b7e70
DillonDepala <dillondepala@hotmail.com>
2022-12-27 12:35:39 +0000

    Created LIVES and GAME_DISPLAY variables

--- constants.py
+++ constants.py
@@ -1,6 +1,8 @@
 import re
 import requests
 WORD_LENGTH = 5
+GAME_DISPLAY = []
+LIVES = 6
 
 meaningpedia_resp = requests.get(
     "https://meaningpedia.com/5-letter-words?show=all")
--- wordle.py
+++ wordle.py
@@ -1,8 +1,6 @@
 import random
 import numpy as np
-from constants import WORD_LENGTH, ACCEPTABLE_WORDS_LIST
-
-BLANK_ARRAY = np.array([])
+from constants import WORD_LENGTH, ACCEPTABLE_WORDS_LIST, LIVES
 
 def user_input():
     user_choice = input(f'Choose a {WORD_LENGTH} letter word: ').lower()
@@ -12,6 +10,31 @@ def user_input():
         raise Exception("Not a valid word\n please try again")
     else:
         return np.array(list(user_choice))
+
+def green_box_check(user_choice, computer_choice, num):
+    if np.array_equal(user_choice[num], computer_choice[num]):
+        return 'Green'
+    
+
+def yellow_box_check(user_choice, computer_choice, num):
+    if user_choice[num] in computer_choice:
+        return 'Yellow'
+
+
+computer_choice = np.array(list(random.choice(ACCEPTABLE_WORDS_LIST)))
+
+print(computer_choice)
+user_choice = user_input()
+
+while LIVES > 0:
+    for num in range(len(user_choice)):
+        if green_box_check(user_choice, computer_choice, num) == 'Green':
+            print('Green')
+        elif yellow_box_check(user_choice, computer_choice, num) == 'Yellow':
+            print('Yellow')
+        else:
+            print('Black')
+    LIVES -= 1
 
 
 
